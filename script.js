59a33d6eff5586fedef3d0dfd21556e0a04d1ac8
dimpr <dmitri.designer@gmail.com>
2022-06-10 12:17:34 +0300

    Lesson 2

--- script.js
+++ script.js
@@ -2,13 +2,18 @@
 const paint = 'растопленный керзовый каблук';
 let body = 'тело бродяги';
 
-let title,
-    screens,
-    screenPrice,
-    rollback,
-    fullPrice,
-    adaptive;
+let title = "Придумать какой-нибудь заголовок",
+    screens = "Простые, Сложные, Интерактивные",
+    screenPrice = 5,
+    rollback = 99,
+    fullPrice = 100,
+    adaptive = true;
 
-alert("Вечер в хату");
-
-console.log('Карамельки не найдется?');
+console.log(typeof (title));
+console.log(typeof (fullPrice));
+console.log(typeof (adaptive));
+console.log(screens.length);
+console.log("Стоимость верстки экранов " + screenPrice + " рублей/ долларов/гривен/юани");
+console.log("Стоимость разработки сайта " + fullPrice + " рублей/ долларов/гривен/юани");
+console.log(screens.toLowerCase().split(", "));
+console.log(fullPrice * (rollback / 100));

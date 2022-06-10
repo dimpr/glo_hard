db22af16f23a130b4a63bff750c4558f167565d2
dimpr <dmitri.designer@gmail.com>
2022-06-10 10:30:48 +0300

    New life

---
+++ script.js
@@ -0,0 +1,14 @@
+
+const paint = 'растопленный керзовый каблук';
+let body = 'тело бродяги';
+
+let title,
+    screens,
+    screenPrice,
+    rollback,
+    fullPrice,
+    adaptive;
+
+alert("Вечер в хату");
+
+console.log('Карамельки не найдется?');
